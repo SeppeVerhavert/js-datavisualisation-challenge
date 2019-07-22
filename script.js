d42efd23a20e90d479eeec9049703e8589627f8b
seppe_verhavert <seppe.verhavert@gmail.com>
2019-07-22 14:23:53 +0200

    add demochart below H3

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-let graph1 = document.getElementsByClassName("mw-headline")[1];
-let graph2 = document.getElementsByClassName("mw-headline")[4];
+let header1 = document.getElementsByTagName('h3')[0];
+let canvas= document.createElement('canvas');
+canvas.id="myCanvas";
+header1.appendChild(canvas);
 
-let canvas = document.createElement("canvas");
-canvas.id="myGraph";
-canvas.innerHTML = 'width="400" height="400"';
-graph1.appendChild(canvas);
-
-let canvas2 = document.createElement("canvas");
-canvas2.id="mySecondGraph";
-canvas2.innerHTML = 'width="400" height="400"';
-graph2.appendChild(canvas2);
+let ctx = document.getElementById('myCanvas').getContext('2d');
+let myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    }
+});
