8b32fe465c29190890c6c128160b49e1fbc85492
seppe_verhavert <seppe.verhavert@gmail.com>
2019-07-22 16:08:11 +0200

    create for loop that logs country names + data

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 let header1 = document.getElementsByTagName('h3')[0];
-let canvas= document.createElement('canvas');
-canvas.id="myCanvas";
+let canvas = document.createElement('canvas');
+canvas.id = "myCanvas";
 header1.appendChild(canvas);
 
 let list = table1.childNodes[5].children;
-let cityNames = [];
+let countryNames = [];
 
 for (i = 1; i < list.length; i++) {
-    cityNames.push(list[i].children[1].firstChild.data);
+    countryNames.push(list[i].children[1].firstChild.data);
+
+    for (j = 1; j < list[i].children.length; j++)
+        console.log(list[i].children[j].innerText);
 }
+
+// console.log(list[1].children[j].innerText);
+// console.log(cityNames);
