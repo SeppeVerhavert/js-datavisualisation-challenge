6c92cf43e7db7b3aaced0f3e31cc32484518d974
seppe_verhavert <seppe.verhavert@gmail.com>
2019-07-22 11:31:39 +0200

    add graph1 canvas & graph2 canvas

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-document.getElementById("table1").innerHTML = "yay";
+let graph1 = document.getElementsByClassName("mw-headline")[1];
+let graph2 = document.getElementsByClassName("mw-headline")[4];
 
-document.getElementById("table2").innerHTML = "yay";
+let canvas = document.createElement("canvas");
+canvas.id="myGraph";
+canvas.innerHTML = 'width="400" height="400"';
+graph1.appendChild(canvas);
+
+let canvas2 = document.createElement("canvas");
+canvas2.id="mySecondGraph";
+canvas2.innerHTML = 'width="400" height="400"';
+graph2.appendChild(canvas2);
