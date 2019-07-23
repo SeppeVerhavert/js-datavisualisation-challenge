39aba449585e8b691eec4640b1ce4d806efa9af2
seppe_verhavert <seppe.verhavert@gmail.com>
2019-07-23 09:25:51 +0200

    made 2d array for data

--- script.js
+++ script.js
@@ -4,16 +4,43 @@ canvas.id = "myCanvas";
 header1.appendChild(canvas);
 
 let list = table1.childNodes[5].children;
-let countryNames = [];
+let countryNames = new Array();
 
 for (i = 1; i < list.length; i++) {
-    array = [];
+    countryNames[i] = new Array();
 
     for (j = 1; j < list[i].children.length; j++) {
-        array.push(list[i].children[j].innerText)
+        countryNames[i].push(list[i].children[j].innerText)
     }
-
-    countryNames.push(array);
 }
 
-console.log(countryNames);
+console.log(countryNames[3][4]);
+
+var ctx = document.getElementById('myCanvas').getContext('2d');
+var myBarChart = new Chart(ctx, {
+    type: 'horizontalBar',
+    data: {
+        labels: ['2002', '2003', '2004', '2005', '2006', '2007'],
+        datasets: [{
+            label: 'Belgium',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+});
