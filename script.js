237786d238c48667b6f8ab26bbeffe269421c4f3
seppe_verhavert <seppe.verhavert@gmail.com>
2019-07-24 10:27:17 +0200

    add : empty cell exception

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ for (l = 1; l < list.length; l++) {
     list[l].appendChild(node);
 
     let checkBox = document.getElementsByClassName("checkBox")[l - 1];
-    checkBox.addEventListener('click', displayCountry);
+    checkBox.addEventListener('click', displayData);
 }
 
 //----------------------------------    LISTS   ------------------------------------------//
@@ -35,59 +35,63 @@ let display = false;
 
 //----------------------------------    DISPLAY COUNTRY     -------------------------------//
 
-function displayCountry() {
+function displayData() {
     let checkedBox = this;
     if (!display) {
-        addCountry(checkedBox);
+        addData(checkedBox);
         drawChart();
         display = true;
     }
     else if (display) {
-        removeCountry(checkedBox);
+        removeData(checkedBox);
         drawChart();
         display = false;
     }
 }
 
-//----------------------------------    ADD COUNTRY     ----------------------------------//
+//----------------------------------    ADD DATA     ----------------------------------//
+
+function addData(checkedBox) {
+    addCountry(checkedBox);
+    addDataSet(checkedBox);
+}
 
 function addCountry(checkedBox) {
     let newCountry = checkedBox.parentNode.parentNode.children[1].innerHTML;
     countryName.push(newCountry);
+}
 
+function addDataSet(checkedBox) {
     let newDataSet = checkedBox.parentNode.parentNode.children;
     for (i = 2; i < newDataSet.length - 1; i++) {
         let newInteger = parseFloat((newDataSet[i].innerHTML).replace(',', '.'));
+        if (isNaN(newInteger)) { newInteger = 0; }
         numberList.push(newInteger);
     }
 }
 
-//----------------------------------    REMOVE COUNTRY     -------------------------------//
+//----------------------------------    REMOVE DATA     -------------------------------//
+
+function removeData(checkedBox) {
+    removeCountry(checkedBox);
+    removeDataSet(checkedBox);
+}
 
 function removeCountry(checkedBox) {
     let oldCountry = checkedBox.parentNode.parentNode.children[1].innerHTML;
     let indexCountry = countryName.indexOf(oldCountry);
     countryName.splice(indexCountry, indexCountry + 1);
+}
 
+function removeDataSet(checkedBox) {
     let oldDataSet = checkedBox.parentNode.parentNode.children;
     for (i = 2; i < oldDataSet.length - 1; i++) {
         let oldData = parseFloat(oldDataSet[i].innerHTML.replace(',', '.'));
-        console.log(oldData);
+        if (isNaN(oldData)) { oldData = 0; }
         let indexData = numberList.indexOf(oldData);
-        console.log(indexData);
         numberList.splice(indexData, indexData + 1);
-        console.log(numberList);
     }
-    
-
-    // let newData = checkedBox.parentNode.parentNode.children;
-    // for (i = 2; i < newData.length - 1; i++) {
-    //     let newInteger = parseInt(newData[i].innerHTML);
-    //     numberList.push(newInteger);
-    // }
 }
-
-//-----------------------------------   CHART   -------------------------------------------//
 
 //-----------------------------------   CHART   -------------------------------------------//
 
